60af15274ec017856adf77c02b38b0972082a762
Jonatas Alves <143411776+JonatasAlv3s@users.noreply.github.com>
2024-12-03 14:56:24 -0300

    Feature/Dashboard
    Create a layout in the dashboard the pages.

--- react-crud/src/routes/index.tsx
+++ react-crud/src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useDrawerContext } from "../shared/contexts/DrawerContext";
 import { useEffect } from "react";
+import { Dashboard } from "../Pages";
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+  const { setDrawerOptions } = useDrawerContext();
 
   useEffect(() => {
     setDrawerOptions([
@@ -18,14 +18,7 @@ export const AppRoutes = () => {
 
   return (
     <Routes>
-      <Route
-        path="/pagina-inicial"
-        element={
-          <Button onClick={toggleDrawerOpen} variant="contained" color="primary">
-            Drawer
-          </Button>
-        }
-      />
+      <Route path="/pagina-inicial" element={<Dashboard />} />
       <Route path="*" element={<Navigate to={"/pagina-inicial"} />} />
     </Routes>
   );
--- react-crud/src/shared/components/menu-lateral/MenuLateral.tsx
+++ react-crud/src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,33 +1,46 @@
 import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material";
-import { useDrawerContext } from "../../contexts";
+import { useDrawerContext } from "../../contexts"; //contexto personalizado para o menu do drawer 
 import { useMatch, useNavigate, useResolvedPath } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 
-interface IMenuLateralProps {
+interface IMenuLateralProps {  //Interface para definir que o o componente aceita filhos
     children: React.ReactNode;
 }
 
 interface IListItemLinkProps {
-    to: string;
-    icon: string;
-    label: string;
-    onClick: (() => void) | undefined;
+    to: string;    //caminho para rota de navegação 
+    icon: string;  //item para o exibição menu
+    label: string; //texto a ser exibido no menu 
+    onClick: (() => void) | undefined;   //funçao ao ser chamada ao clicar 
 }
 
+//componente para cada item do menu 
 const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }) => {
-    const navigate = useNavigate();
+    const navigate = useNavigate();  //hook para navegação de rota 
 
-    const resolvedPath = useResolvedPath(to);
-    const match = useMatch({ path: resolvedPath.pathname, end: false });
+    const resolvedPath = useResolvedPath(to);  //resolvedpath cria um objeto com o camimnho absoluto da rota 
+    const match = useMatch({ path: resolvedPath.pathname, end: false }); //usematch verifica o caminho atual da rota 
+    const [isSelected, setIsSelected] = useState(!!match);  //controla se o item esta selecionado 
 
+    //atualiza sempre que o estado atual da rota muda 
+    useEffect(() => {
+        if (match) {
+            setIsSelected(true);
+        }
+    }, [match])
+
+    //função chamada ao clicak em algum item do menu 
     const handleClick = () => {
-        navigate(to);
-        onClick?.();
+        setIsSelected(true);  //marca o item selecionado 
+        navigate(to);         //define o camimho da rota 
+        onClick?.();          //chama a função onclick passada como props 
     }
 
     return (
-        <ListItemButton selected={!!match} onClick={handleClick}>
+        //renderiza  o botao com base no estilo selecionado baseado no estado 
+        <ListItemButton selected={isSelected} onClick={handleClick}>
             <ListItemIcon>
                 <Icon>{icon}</Icon>
             </ListItemIcon>
@@ -36,10 +49,12 @@ const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }
     );
 }
 
+//componente principal do menu lateral 
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
-    const theme = useTheme();
-    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
-    const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
+    const theme = useTheme();  //hook do material para acesar o tema
+    const smDown = useMediaQuery(theme.breakpoints.down('sm')); //detecta o tamanho da tela
+    const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();  //dados e funções personalizados
+
     return (
         <>
             <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
